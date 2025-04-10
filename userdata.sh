4eadeae591fba12d382fe3bec0ec6da41c5a4ae7
Jennifer Alicea <aliceaj@moravian.edu>
2025-04-10 08:23:41 -0400

    add execution permission

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install git -y
-git clone https://github.com/cs220s25/alicea_cicd/alicea_cicd
+git clone https://github.com/cs220s25/alicea_cicd /alicea_cicd
 cd /alicea_cicd
 python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt 
